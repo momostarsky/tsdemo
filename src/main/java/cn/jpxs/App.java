68ffc0a7fbd2913c2ca1ad7b2442fbb5199341c9
momo.starsky <momo.starsky@qq.com>
2021-05-20 09:45:42 +0800

    添加版本号!

--- src/main/java/cn/jpxs/App.java
+++ src/main/java/cn/jpxs/App.java
@@ -1,14 +1,15 @@
 package cn.jpxs;
-import org.dcm4che3.data.Attributes;
 
 import org.dcm4che3.imageio.plugins.dcm.DicomImageReadParam;
 import org.dcm4che3.io.DicomInputStream;
+import org.opencv.core.Core;
+
 import javax.imageio.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
-import static java.lang.System.loadLibrary;
+
 
 /**
  * Hello world!
@@ -21,17 +22,17 @@ public class App
         String lib = System.getProperty("java.library.path");
         lib = "./target/classes;" + lib;
         System.setProperty("java.library.path", lib);
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
     }
 
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println("Currrent Working Directory:"+ System.getProperty("user.dir") );
         ImageReader imageReader =  ImageIO.getImageReadersByFormatName("DICOM").next();
         File file =new File("/home/dhz/dcmdata/1.dcm");
         try (DicomInputStream dis = new DicomInputStream(file)) {
             imageReader.setInput(dis);
-            DicomImageReadParam param =
-                    (DicomImageReadParam) imageReader.getDefaultReadParam();
+            DicomImageReadParam param = (DicomImageReadParam) imageReader.getDefaultReadParam();
              BufferedImage imgx = imageReader.read( 0, param);
              System.out.println( imgx != null);
             System.out.println( imgx.getWidth() +"X"+ imgx.getHeight());
